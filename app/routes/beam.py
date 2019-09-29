4947df3828116354cb6c9d07de5bdc7d8fe16d03
Alexius Academia <alexius.academia@gmail.com>
2019-09-29 11:10:17 +0800

    Checking for main section nodes

--- app/routes/beam.py
+++ app/routes/beam.py
@@ -12,10 +12,17 @@ def beam_balanced_analysis():
 
     _main_section = []
 
-    if args['main_section']:
-        for _ms in args['main_section']:
-            _node: Node = Node(_ms[0], _ms[1])
-            _main_section.append(_node)
+    _main_section_args = args['main_section']
+
+    if not _main_section_args:
+        return has_error('Main section nodes not defined.')
+
+    if len(_main_section_args) < 3:
+        return has_error('Main section nodes must be equal or more than 3.')
+
+    for _ms in args['main_section']:
+        _node: Node = Node(_ms[0], _ms[1])
+        _main_section.append(_node)
 
     print(_main_section)
 
